8ea4f33812ebc641e48c5f2e4fc526dc67b7b1cf
Franck6ix <fga.groupentreprise@gmail.com>
2025-10-22 11:47:02 -0400

    component

--- resources/views/components/heros-section.blade.php
+++ resources/views/components/heros-section.blade.php
@@ -1,3 +1,26 @@
-<div>
-    <!-- We must ship. - Taylor Otwell -->
-</div>
+                    <section class="bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+
+                <div class="container mx-auto px-4 py-24 ">
+
+            <div class="max-w-3xl">
+
+        <h1 class="text-5xl font-bold mb-6 ">Découvrer des articles</h1>
+    <p class="text-xl mb-8 text-blue-100">
+
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi cupiditate porro ipsa delectus odio vitae, repudiandae unde officiis facilis doloribus, laudantium hic fuga sed molestiae? Est nobis ducimus in animi!  
+
+    </p>
+
+        <a href="#produits" class="inline-block bg-white text-blue-600 font-semibold px-8 py-3 rounded-lg hover:bg-gray-100 transition">
+        Explorer maintenant
+            </a>
+
+                </div>
+
+
+                    </div>
+
+
+
+
+                        </section>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,5 @@
 @extends('Layout.app')
 
 @section('content')
-    <h1>Entrainement Au MVC </h1>
 
-    <p>ZONE ACTIVE </p><br>
-
-   
 @endsection
-
