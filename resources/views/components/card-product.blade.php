6f5e0eacc2cffcfdf5516adf6a08086cae2e40ba
Franck6ix <fga.groupentreprise@gmail.com>
2025-10-23 12:13:57 -0400

    ajout des grilles

--- resources/views/components/card-product.blade.php
+++ resources/views/components/card-product.blade.php
@@ -1,15 +1,32 @@
 
 <!--CARD PRODUIT-->
 
-<div class="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-2xl transition ">
+<div class="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition ">
         
-        <div class="bg-amber-600 h-64 flex items-center justify-center">
-        <img>
+
+
+        <div class="bg-gray-300 h-64 flex items-center justify-center">
+
                 Image
         </div>
         
         
-        Ma Card
+        Zone de separation
+        
+        <div class=" text-black h-20 p-4 ">
 
+
+                
+                <span class="text-xm text-orange-600 font-bold">Categorie:</span>
+                <h3 class="lg font-bold mt-2 mb-2 text-gray-800">nom Product</h3>
+                <p class="text-xm text-gray-600 mb-4 ">Article de type Prenium</p>
+
+                <div class="flex items-center justify-between">
+                        <span class="text-2xl font-bold text-blue-600">Prix</span>
+                        <button class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-colors">Ajouter</button>
+                </div>
+                
+        
+        </div>
         
 </div>
