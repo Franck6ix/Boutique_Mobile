6f4521fa6f195b951780210e2d89dd54e0d17446
Franck6ix <fga.groupentreprise@gmail.com>
2025-10-23 08:32:21 -0400

    Push Version

--- resources/views/components/heros-section.blade.php
+++ resources/views/components/heros-section.blade.php
@@ -4,14 +4,14 @@
 
             <div class="max-w-3xl">
 
-        <h1 class="text-5xl font-bold mb-6 ">Découvrer des articles</h1>
+        <h1 class="text-5xl font-bold mb-6 ">Découvrer les articles</h1>
     <p class="text-xl mb-8 text-blue-100">
 
-    Un ensemble d'articles qui vous satisferont , par leur qualité et leur originalité. Habillez vous avec les yeux, acheter avec le coeur, reparté Heureux et Renouveau.
+    Un ensemble d'articles qui vous satisferont , par leur qualité et leur originalité. Habillez votre techno, , reparté Heureux et Renouveau.
     </p>
 
         <a href="#produits" class="inline-block bg-white text-blue-600 font-semibold px-8 py-3 rounded-lg hover:bg-gray-100 transition">
-        Explorer maintenant
+        Explorer maintenant 
             </a>
 
                 </div>
