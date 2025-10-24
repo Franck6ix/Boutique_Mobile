2718b98677c888dedf124f514c702be688d69351
Franck6ix <fga.groupentreprise@gmail.com>
2025-10-24 12:27:01 -0400

    remise en ligne de la boutique

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use    App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BoutiqueController;
+use    App\Http\Controllers\ProductController;
 
+Route::get('/Ma_Boutique', [BoutiqueController::class, 'index'])->name('detail');
 Route::get('/', function () {
     return view('welcome');
 });
